d3b8ab87ed270a20faed5fa1c61bcb9852b02f76
Marcin Floryan <mfloryan@spotify.com>
2016-12-16 08:23:43 +0100

    Day 15 part one solved

---
+++ day15.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let simpleInput = [
+    'Disc #1 has 5 positions; at time=0, it is at position 4.',
+    'Disc #2 has 2 positions; at time=0, it is at position 1.'
+];
+
+let input = [
+    'Disc #1 has 7 positions; at time=0, it is at position 0.',
+    'Disc #2 has 13 positions; at time=0, it is at position 0.',
+    'Disc #3 has 3 positions; at time=0, it is at position 2.',
+    'Disc #4 has 5 positions; at time=0, it is at position 2.',
+    'Disc #5 has 17 positions; at time=0, it is at position 0.',
+    'Disc #6 has 19 positions; at time=0, it is at position 7.'
+];
+
+
+function parseSetup(input) {
+    let setup = {};
+    input.forEach(l => {
+        setup[Number.parseInt(l.match(/#(.)/)[1])] = {
+            size: Number.parseInt(l.match(/has (.+) positions/)[1]),
+            offset: Number.parseInt(l.match(/it is at position (.+)./)[1])
+        };
+    });
+    return setup;
+}
+
+let setup = parseSetup(input);
+console.log(setup);
+
+let aligned = false;
+let t = 0;
+
+let setupKeys = Object.keys(setup);
+do {
+    aligned = setupKeys
+            .map(key => (t + Number.parseInt(key) + setup[key].offset) % setup[key].size)
+            .reduce((p,c) => p+c, 0) == 0;
+    t++;
+} while (!aligned);
+
+console.log(t-1);
