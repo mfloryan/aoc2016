a1073d319181b0146788e334f0bb4d85ec73175f
Marcin Floryan <mfloryan@spotify.com>
2016-12-19 07:20:32 +0100

    Day 19: example solved

---
+++ day19-elves.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let elves = 5;
+let elvesWithPresents = Array(elves).fill(1).map((p,i) => {return {e:i+1, p:p};});
+
+function movePresentsOnce(circle) {
+    for (let i=0; i < circle.length; i++) {
+        let next = (i+1)%circle.length;
+        if (circle[i].p > 0) {
+            circle[i].p += circle[next].p;
+            circle[next].p = 0;
+        }
+    }
+    return circle;
+}
+
+while (elvesWithPresents.length > 1) {
+    movePresentsOnce(elvesWithPresents);
+    elvesWithPresents = elvesWithPresents.filter(e => e.p > 0);
+}
+
+console.log(elvesWithPresents[0].e);
