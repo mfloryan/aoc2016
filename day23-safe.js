cf4a801ea919307dcf62151e33b550851e46636a
Marcin Floryan <mfloryan@spotify.com>
2016-12-27 20:13:39 +0100

    Day 23: Computer from day12 in a Class

--- day23-safe.js
+++ day23-safe.js
@@ -1,82 +1,81 @@
 "use strict";
 
 let assembunny = [
-    'cpy 41 a',
-    'inc a',
-    'inc a',
+    'cpy 2 a',
+    'tgl a',
+    'tgl a',
+    'tgl a',
+    'cpy 1 a',
     'dec a',
-    'jnz a 2',
-    'dec a'
+    'dec a',
 ];
 
 let assembunny2 = [
-    'cpy 1 a',
-    'cpy 1 b',
-    'cpy 26 d',
-    'jnz c 2',
-    'jnz 1 5',
-    'cpy 7 c',
-    'inc d',
-    'dec c',
-    'jnz c -2',
-    'cpy a c',
-    'inc a',
-    'dec b',
-    'jnz b -2',
-    'cpy c b',
-    'dec d',
-    'jnz d -6',
-    'cpy 16 c',
-    'cpy 17 d',
-    'inc a',
-    'dec d',
-    'jnz d -2',
-    'dec c',
-    'jnz c -5'
 ];
 
-function execute(code) {
-    let registers = {a:0, b:0, c:1, d:0};
-    let pc = 0;
+class Computer {
 
-    while (pc < code.length) {
-        let instruction = code[pc].split(' ');
-        if (instruction[0] == 'cpy') {
-            let value = Number.parseInt(instruction[1]);
-            if (Number.isNaN(value)) {
-                value = registers[instruction[1]];
-            }
-            let register = instruction[2];
-            registers[register] = value;
-            pc++;
+    cpy(valueOrRegister, register) {
+        let value = Number.parseInt(valueOrRegister);
+        if (Number.isNaN(value)) {
+            value = this.registers[value];
         }
-
-        if (instruction[0] == 'inc') {
-            registers[instruction[1]]++;
-            pc++;
-        }
-
-        if (instruction[0] == 'dec') {
-            registers[instruction[1]]--;
-            pc++;
-        }
-
-        if (instruction[0] == 'jnz') {
-            let value = Number.parseInt(instruction[1]);
-            if (Number.isNaN(value)) {
-                value = registers[instruction[1]];
-            }
-            if (value != 0) {
-                pc += Number.parseInt(instruction[2]);
-            } else {
-                pc++;
-            }
-        }
-        // console.log(pc);
+        this.registers[register] = value;
+        this.pc++;
     }
 
-    return registers;
+    inc(register) {
+        this.registers[register]++;
+        this.pc++;
+    }
+
+    dec(register) {
+        this.registers[register]--;
+        this.pc++;
+    }
+
+    jnz(valueOrRegister, jump) {
+        let value = Number.parseInt(valueOrRegister);
+        if (Number.isNaN(value)) {
+            value = this.registers[valueOrRegister];
+        }
+
+        if (value != 0) {
+            this.pc += Number.parseInt(jump);
+        } else {
+            this.pc++;
+        }
+    }
+
+    constructor(assemblyCode) {
+        this.registers = {a:0, b:0, c:0, d:0};
+        this.pc = 0;
+        this.code = [];
+
+        assemblyCode.forEach( i => {
+            let args = i.split(' ');
+            let op = args.shift();
+            if (typeof(this[op]) === "function") {
+                this.code.push({f: this[op], a: args});
+            } else {
+                console.log("Unidentified assembly: " + i);
+            }
+        });
+    }
+
+    execute(initialRegisters) {
+        this.registers = Object.assign(this.registers, initialRegisters);
+        while (this.pc < this.code.length) {
+            let currentOperation = this.code[this.pc];
+            currentOperation.f.apply(this, currentOperation.a);
+            console.log(this.pc);
+        }
+        return this.registers;
+    }
 }
 
+let computer = new Computer(assembunny);
 
-console.log(execute(assembunny2));
+console.log(
+    computer.execute({a:7})
+);
